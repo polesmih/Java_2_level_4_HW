1f5c3b5ed338ebd7bc285127ac51f226ecfb82da
polesmih <polesmih@yandex.ru>
2021-03-31 18:19:40 +0300

    Homework lesson 7 level 2

---
+++ src/chat/ClientOne.java
@@ -0,0 +1,9 @@
+package chat;
+
+import chat.client.ChatClient;
+
+public class ClientOne {
+    public static void main(String[] args) {
+        new ChatClient();
+    }
+}
---
+++ src/chat/ClientThree.java
@@ -0,0 +1,9 @@
+package chat;
+
+import chat.client.ChatClient;
+
+public class ClientThree {
+    public static void main(String[] args) {
+        new ChatClient();
+    }
+}
